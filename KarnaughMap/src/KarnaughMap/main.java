0cf1c91ab2ec518d730fe247c4a2f77f251bdf03
MahmudMardini <61077886+MahmudMardini@users.noreply.github.com>
2020-08-10 14:49:42 +0300

    delete unimportant .class files

--- KarnaughMap/src/KarnaughMap/main.java
+++ KarnaughMap/src/KarnaughMap/main.java
@@ -7,7 +7,7 @@ public class main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner (System.in);
 		
-		System.out.println("L�tfen Boole fonksiyonunu giriniz:");
+		System.out.println("Lütfen Boole fonksiyonunu giriniz:");
 		String str = s.nextLine();
 		KarnaughMap test = new KarnaughMap(str);
 	}
